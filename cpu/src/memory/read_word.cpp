ba9b711635f7f8f3422c6b58df4d8a9ad19e6142
Benjamin <benjamin.l.renz@gmail.com>
2026-09-02 22:10:09 +0200

    Added bound checks to memory word access

--- cpu/src/memory/read_word.cpp
+++ cpu/src/memory/read_word.cpp
@@ -1,7 +1,14 @@
 #include <cstdint>
 #include <octo/memory/memory.hpp>
+#include <octo_log.h>
 
 std::uint16_t Memory::read_word(std::uint16_t high_address) {
+
+	if (high_address >= 0xFFFE) {
+		log_error("[Memory]", "Memory adress out of bounds");
+		return 0;
+	}
+
 	auto high = this->read(high_address);
 	auto low = this->read(high_address + 1);
 
--- cpu/src/memory/write_word.cpp
+++ cpu/src/memory/write_word.cpp
@@ -1,7 +1,13 @@
 #include <cstdint>
 #include <octo/memory/memory.hpp>
+#include <octo_log.h>
 
 void Memory::write_word(std::uint16_t high_address, std::uint16_t value) {
+	if (high_address >= 0xFFFE) {
+		log_error("[Memory]", "Memory adress out of bounds");
+		return;
+	}
+
 	std::uint8_t high = value >> 8;
 	std::uint8_t low = value & 0xFF;
 
